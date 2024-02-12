50c558533ce87a7f0f825e0b2530c415c4a320c7
Dogaru Stefan <7stefanadrian@gmail.com>
2024-02-12 14:47:12 +0200

    second commit

--- src/main/java/com/example/licentav1/controller/StudentsController.java
+++ src/main/java/com/example/licentav1/controller/StudentsController.java
@@ -19,5 +19,8 @@ public class StudentsController {
         return studentsService.getStudents();
     }
 
-
+    @PostMapping("/students")
+    public void addStudent(Students student) {
+        studentsService.addStudent(student);
+    }
 }
