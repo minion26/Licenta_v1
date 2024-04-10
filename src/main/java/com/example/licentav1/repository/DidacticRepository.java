47e8bf74a4c84ba181565c48de6c382d589f103f
Jitca Diana <jitcadiana6@gmail.com>
2024-04-10 21:55:35 +0300

    scoring the correct answers : commited forgotten file

--- src/main/java/com/example/licentav1/repository/DidacticRepository.java
+++ src/main/java/com/example/licentav1/repository/DidacticRepository.java
@@ -1,11 +1,21 @@
 package com.example.licentav1.repository;
 
 import com.example.licentav1.domain.Didactic;
+import jakarta.validation.constraints.Past;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DidacticRepository extends JpaRepository<Didactic, UUID> {
+    @Query(value="SELECT * FROM didactic WHERE id_courses = :idCourses", nativeQuery = true)
+    Optional<Didactic> findByIdCourses(@Param("idCourses") UUID idCourses);
+
+    @Query(value="SELECT * FROM didactic WHERE id_courses = :idCourses", nativeQuery = true)
+    Optional<List<Didactic>> findAllByIdCourses(@Param("idCourses") UUID idCourses);
 }
