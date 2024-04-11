a872093211a108fc544c3c82dd2506118b840a9b
Jitca Diana <jitcadiana6@gmail.com>
2024-04-11 22:06:21 +0300

    aws : preparing for storing homework files

--- src/main/java/com/example/licentav1/AWS/S3Service.java
+++ src/main/java/com/example/licentav1/AWS/S3Service.java
@@ -25,6 +25,9 @@ public class S3Service {
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
 
+    @Value("${aws.s3.bucket-name-2}")
+    private String bucketName2;
+
     public S3Service(AmazonS3 s3Client) {
         this.s3Client = s3Client;
     }
@@ -72,6 +75,47 @@ public class S3Service {
 
     public void deleteFile(String fileName) {
         s3Client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
+    }
+
+
+
+    // Added for the second bucket
+    public List<S3ObjectSummary> listHomeworks() {
+        ObjectListing objectListing = s3Client.listObjects(bucketName2);
+        return objectListing.getObjectSummaries();
+    }
+
+    public String uploadHomeworkFile(MultipartFile file) throws IOException, InterruptedException {
+
+        File tempFile = File.createTempFile("temp-file-name", ".tmp");
+        file.transferTo(tempFile);
+
+        TransferManager tm = TransferManagerBuilder.standard()
+                .withS3Client(this.getS3Client())
+                .withMultipartUploadThreshold((long) (5 * 1024 * 1025))
+                .build();
+
+        Upload upload = tm.upload(bucketName2, file.getOriginalFilename(), tempFile);
+        upload.waitForCompletion();
+
+        tempFile.delete();
+
+        String fileUrl = bucketName2 + "/" + file.getOriginalFilename();
+        return fileUrl;
+    }
+
+    public S3Object downloadHomeworkFile(String fileName) {
+
+        S3Object s3Object =  s3Client.getObject(new GetObjectRequest(bucketName2, fileName));
+        if (s3Object == null) {
+            throw new AmazonS3Exception("File not found: " + fileName);
+        }
+        return s3Object;
+
+    }
+
+    public void deleteHomeworkFile(String fileName) {
+        s3Client.deleteObject(new DeleteObjectRequest(bucketName2, fileName));
     }
 
 }
